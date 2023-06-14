68b927d64f053a0140b868884619ab2550c75e26
Konrad Gadzina <konrad@enklawa.blog>
2023-06-14 16:43:51 +0200

    Add reseting of transform

--- Assets/TransformReseter.cs
+++ Assets/TransformReseter.cs
@@ -6,17 +6,30 @@ public class TransformReseter : MonoBehaviour
 {
     public Transform transformToReset;
     public Vector3 startingPositon;
-    public Vector3 startingRotation;
+    public Vector3 startingRotationAngles;
+    public Quaternion startingRotation;
     // Start is called before the first frame update
     void Start()
     {
+        if (transformToReset == null)
+        {
+            transformToReset = transform;
+        }
+
         startingPositon = transformToReset.position;
-        // startingRotation = transformToReset.rotation;
+        startingRotation = transformToReset.rotation;
+        startingRotationAngles = startingRotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("jestok");
+            transformToReset.position = startingPositon;
+            startingRotation.eulerAngles = startingRotationAngles;
+            transformToReset.rotation = startingRotation;
+        }
     }
 }
